aa28a0b775cd879fb51a37e43828f6ea654c08a2
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2019-10-24 22:07:16 +0300

    update helpers

--- 2/helper.py
+++ 2/helper.py
@@ -1,20 +1,22 @@
-def cycled_input(text, input_type, valid_cond):
-    input_type = input_type.__name__
+import re
 
-    while True:
-        try:
-            if input_type == "int":
-                user_input = int(input(text))
-            elif input_type == "float":
-                user_input = float(input(text))
+PATTERNS = {
+    int: r"^[-+]?[0-9]+$",
+    float: r"[+-]?([0-9]*[.])?[0-9]+",
+    str: r".+",
+}
+
+
+def cycled_input(text: str, input_type=str, valid_cond=None):
+    if input_type in PATTERNS:
+        while True:
+            user_input = input(text)
+            if bool(re.fullmatch(PATTERNS[input_type], user_input)):
+                if (valid_cond is not None and valid_cond(input_type(user_input)) is True) or valid_cond is None:
+                    return input_type(user_input)
+                else:
+                    print("input value is not valid.")
             else:
-                user_input = input(text)
-        except ValueError:
-            print("You entered wrong value. Let's try again.")
-            continue
-
-        if valid_cond is not None and valid_cond(user_input) is not True:
-            print("input value is not valid.")
-        else:
-            return user_input
-    return
+                print("You entered wrong value. Let's try again.")
+    else:
+        return None
