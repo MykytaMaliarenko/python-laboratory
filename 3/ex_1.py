c4f48d8e342fc5cfe8ec3edcf3779eb0f47fe7c3
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2019-11-07 21:46:24 +0200

    minor changes

--- 3/ex_1.py
+++ 3/ex_1.py
@@ -1,21 +1,20 @@
 from python_helpers import helpers
 
-CHARS_TO_REMOVE = ["a", "а"]
 SEPARATOR = " "
 
 
 @helpers.cycled
 def main():
-    text = helpers.cycled_input("Enter text:\n", str, lambda v: len(v) > 0)
-    word_length = helpers.cycled_input("Enter word length:\n", int, lambda v: v > 0)
+    text = helpers.cycled_input("Enter text:", str, lambda v: len(v) > 0)
+    word_length = helpers.cycled_input("Enter word length:", int, lambda v: v > 0)
+    char_to_remove = helpers.cycled_input("Char to remove:", str, lambda v: len(v) == 1)
 
     words = text.split(SEPARATOR)
     for i in range(0, len(words)):
         if len(words[i]) == word_length:
-            for char in CHARS_TO_REMOVE:
-                words[i] = words[i].replace(char, "")
+            words[i] = words[i].replace(char_to_remove, "")
 
-    print("Res:\n", SEPARATOR.join(words))
+    print("Res:", SEPARATOR.join(words))
 
 
 if __name__ == "__main__":
